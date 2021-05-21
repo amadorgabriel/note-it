9767cc768ffc8b3c788a512fe4451fca4e01a37c
Gabriel Amador <gabrielramador2014@gmail.com>
2021-05-21 03:49:46 -0300

    feat: improved functions structure

--- src/utils/monthDaysManagement.ts
+++ src/utils/monthDaysManagement.ts
@@ -1,7 +1,6 @@
 import lastDayOfMonth from "date-fns/lastDayOfMonth";
 import addMonths from "date-fns/addMonths";
 import subMonths from "date-fns/subMonths";
-import format from "date-fns/format"
 
 export function getAllDaysOfMonth(month?: Date) {
   if (!month) {
@@ -11,10 +10,6 @@ export function getAllDaysOfMonth(month?: Date) {
   const lastDayOfCurrentMonth: number = parseInt(
     lastDayOfMonth(month).toLocaleDateString().split("/")[0]
   );
-  const monthNumber: number = parseInt(
-    lastDayOfMonth(month).toLocaleDateString().split("/")[1]
-  );
-  const monthName = lastDayOfMonth(month);
 
   var daysOfMonth: string[] = [];
 
@@ -25,28 +20,31 @@ export function getAllDaysOfMonth(month?: Date) {
       daysOfMonth[i] = daysOfMonth[i].padStart(2, "0");
   }
 
-  return [daysOfMonth, monthNumber, monthName];
+  return [daysOfMonth];
 }
 
 function getDaysAndMonthArray(allDays, currentMonthNumber) {
   const lastDayOfCurrentMonth: number = parseInt(
     lastDayOfMonth(new Date()).toLocaleDateString().split("/")[0]
-    );
-    
+  );
+
   const lastDayOfPreviousMonth: number = parseInt(
     lastDayOfMonth(subMonths(new Date(), 1)).toLocaleDateString().split("/")[0]
   );
-  
+
   const lastDayOfNextMonth: number = parseInt(
     lastDayOfMonth(addMonths(new Date(), 1)).toLocaleDateString().split("/")[0]
-    );
-    const currentYear = new Date().getFullYear()
+  );
+  const currentYear = new Date().getFullYear();
 
   let newArray: [[any, number, Date]] = [[null, null, null]];
 
-
   for (let i = 0; i < lastDayOfPreviousMonth; i++) {
-    newArray[i] = [allDays[i], currentMonthNumber - 1, new Date(currentYear, currentMonthNumber - 1, allDays[i])];
+    newArray[i] = [
+      allDays[i],
+      currentMonthNumber - 1,
+      new Date(currentYear, currentMonthNumber - 2, allDays[i]),
+    ];
   }
 
   for (
@@ -54,7 +52,11 @@ function getDaysAndMonthArray(allDays, currentMonthNumber) {
     i < lastDayOfPreviousMonth + lastDayOfCurrentMonth;
     i++
   ) {
-    newArray[i] = [allDays[i], currentMonthNumber, new Date(currentYear, currentMonthNumber, allDays[i])];
+    newArray[i] = [
+      allDays[i],
+      currentMonthNumber,
+      new Date(currentYear, currentMonthNumber - 1, allDays[i]),
+    ];
   }
 
   for (
@@ -62,10 +64,12 @@ function getDaysAndMonthArray(allDays, currentMonthNumber) {
     i < lastDayOfPreviousMonth + lastDayOfCurrentMonth + lastDayOfNextMonth;
     i++
   ) {
-    newArray[i] = [allDays[i], currentMonthNumber + 1, new Date(currentYear, currentMonthNumber + 1, allDays[i])];
+    newArray[i] = [
+      allDays[i],
+      currentMonthNumber + 1,
+      new Date(currentYear, currentMonthNumber, allDays[i]),
+    ];
   }
-
-  console.log(newArray);
 
   return newArray;
 }
@@ -79,6 +83,7 @@ export function concatMonthsArray(previoustMonth, currentMonth, nextMonth) {
   );
 
   const allDays = previoustMonth.concat(currentMonth.concat(nextMonth));
+  const allDates: Date[] = [null];
   const allDaysAndMonth = getDaysAndMonthArray(allDays, currentMonthNumber);
 
   let currentDayIndex: number;
@@ -90,7 +95,9 @@ export function concatMonthsArray(previoustMonth, currentMonth, nextMonth) {
     ) {
       currentDayIndex = i;
     }
+
+    allDates[i] = allDaysAndMonth[i][2];
   }
 
-  return [allDays, currentDayIndex];
+  return { allDays, currentDayIndex, allDates };
 }
